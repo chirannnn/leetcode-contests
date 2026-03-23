63b40f3a7d146413ce54fcd523ed78bb97f134b3
chirannnn <chiranyt117@gmail.com>
2026-03-23 16:19:41 +0530

    update contest

---
+++ 19-biweekly-contest-178/src/com/contest/FirstUniqueEvenElement_01.java
@@ -0,0 +1,46 @@
+/*
+You are given an integer array nums.
+
+Return an integer denoting the first even integer (earliest by array index) that appears exactly once in nums. If no such integer exists, return -1.
+
+An integer x is considered even if it is divisible by 2.
+
+Example 1:
+Input: nums = [3,4,2,5,4,6]
+Output: 2
+Explanation:
+Both 2 and 6 are even and they appear exactly once. Since 2 occurs first in the array, the answer is 2.
+
+Example 2:
+Input: nums = [4,4]
+Output: -1
+Explanation:
+No even integer appears exactly once, so return -1.
+
+Constraints:
+1 <= nums.length <= 100
+1 <= nums[i] <= 100
+ */
+
+package com.contest;
+
+public class FirstUniqueEvenElement_01 {
+    public static void main(String[] args) {
+        int[] nums = {3,4,2,5,4,6};
+        System.out.println(firstUniqueEven(nums));
+    }
+
+    static int firstUniqueEven(int[] nums) {
+        int[] freq = new int[101];
+        for (int num : nums) {
+            freq[num]++;
+        }
+
+        for (int num : nums) {
+            if (num % 2 == 0 && freq[num] == 1) {
+                return num;
+            }
+        }
+        return -1;
+    }
+}
